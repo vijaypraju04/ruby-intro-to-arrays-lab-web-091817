6d5e6d5b52fe8e04f4af45849e0053565a15779f
Vijay Raju <vijaypraju04@gmail.com>
2017-09-15 23:23:44 +0000

    Basic Arrays

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+def instantiate_new_array
+  newArr = []
+  return newArr
+end
+
+def array_with_two_elements
+  newArr = ["one", "two"]
+  return newArr
+end
+
+def first_element(arr)
+  return arr[0]
+end
+
+def third_element(arr)
+  return arr[2]
+end
+
+def last_element(arr)
+  return arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  return south_east_asia.first
+end
+
+def last_element_with_array_methods(arr)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  return south_east_asia.last
+end
+
+def length_of_array(arr)
+  return arr.length
+end
